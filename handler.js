f275ecd70d387503770a2ff6aaca3c57503c0826
Matt Ross <amsross@gmail.com>
2018-01-09 14:44:03 -0500

    INITIAL COMMIT

---
+++ handler.js
@@ -0,0 +1,52 @@
+'use strict'
+const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
+const express = require('express')
+const AWS = require('aws-sdk')
+const { dbPut, dbScan } = require('./utils')('beer')
+
+const app = express()
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
+const db = new AWS.DynamoDB.DocumentClient()
+
+app.get('/:stage?/help', (req, res) => res.json({
+  response_type: 'in_channel',
+  text: 'Try these commands:',
+  attachments: [{
+    'text': '`beer:list`: List the beers in the list'
+  }, {
+    'text': '`beer:add [beer name]`: Add a beer to the list'
+  }]
+}))
+
+app.post('/:stage?/add', ({ body: { text, team_id, user_id } }, res) => dbPut(db)({
+  Name: text,
+  TeamID: team_id,
+  UserID: user_id
+})
+  .toCallback((err, data) => res.json({
+    response_type: 'in_channel',
+    text: err || data
+  })))
+
+app.get('/:stage?/list', ({ query: { team_id, user_id } }, res) => dbScan(db)({
+  TeamID: team_id,
+  UserID: user_id
+})
+  .collect()
+  .toCallback((err, beers) => res.json({
+    response_type: 'in_channel',
+    text: err.message || 'here are your beers:',
+    attachments: (beers || [])
+  })))
+
+module.exports.handler = serverless(app)
